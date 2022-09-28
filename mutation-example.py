49e798d4af1aa77a4a5149c15289ca608dfd55ad
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-27 20:21:35 -0700

    Update mutation-example.py

--- mutation-example.py
+++ mutation-example.py
@@ -19,14 +19,16 @@ line = input()
 
 indx= int(line.split()[0])
 char = line.split()[1]
-# type 'Hello World'
-# type '6 Z'
+# Input 'Hello World'
+# Input '6 Z'
 indx = line.split()[0]
 char = line.split()[1]
 indx
 # should give an output of '6'
 char
 # should give an output of 'Z'
-
+out = s[:indx] + char + s[indx+1:]
+out
+# should give an Output 'Hello Zorld' 
 
 
