20cbbf29cf15d4e0df9edc2ce72a351fdc671ee1
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-27 20:27:29 -0700

    Create find-substring-example.py

---
+++ find-substring-example.py
@@ -0,0 +1,13 @@
+# Find Substring example
+# In this challenge, user enters string & substring. You must print number of times that substring occurs in given string.
+# Note: String letters are case sensitive
+
+# Input Format: 1st line contains original string. Next tine contains substring
+# Output Format: Integer indicating total number of times substring occurs in give string
+
+# Example:
+# Input:
+# ABCDCD
+# CDC
+# Output:
+# 2
