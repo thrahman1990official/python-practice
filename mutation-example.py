03be557e42c8e43086247e897e33a0095679b01c
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-27 20:08:42 -0700

    Create mutation-example.py

---
+++ mutation-example.py
@@ -0,0 +1,19 @@
+# Mutations
+# Read given string, change character at given index & print modified strong
+# Note: Try changing list, tuple, & string. Understand difference between mutable & immutable datatpes
+
+# Input format: First-line contains the string. Next line contains an integer & character indicating the position at which character should be inserted
+# & character indicating position at which character should be inserted in string
+
+# Output: Modified string
+
+# Example:
+# Input:
+# Hello World
+# 6 Z
+# Output:
+# Hello Zorld
+
+s = input()
+line = input()
+
