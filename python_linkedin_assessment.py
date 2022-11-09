0af8c5636bb3199cd7cca7e88fe3a4477ab19b4c
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-11-09 11:09:37 -0800

    Update python_linkedin_assessment.py

--- python_linkedin_assessment.py
+++ python_linkedin_assessment.py
@@ -9,3 +9,16 @@
 # Q3. What data structure does a binary tree degenerate to if it isn't balanced properly?
 # ANSWER: linked list
 
+# Q4. What statement about static methods is true?
+# ANSWER: Static methods serve mostly as utility methods or helper methods, since they can't access or modify a class's state
+
+# Q5. What are attributes?
+# ANSWER: Attributes are a way to hold data or describe a state for a class or an instance of a class.
+# EXPLANATION: Attributes defined under the class, arguments goes under the functions. arguments usually refer as parameter, whereas attributes are the constructor of the class or an instance of a class.
+
+# Q6. What is the term to describe this code?
+count, fruit, price = (2, 'apple', 3.5)
+# ANSWER: tuple unpacking
+
+# Q7. What built-in list method would you use to remove items from a list?
+# ANSWER: .pop() method
