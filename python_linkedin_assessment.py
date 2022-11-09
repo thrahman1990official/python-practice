71fe17559edf40a4ddf64256178723e17802fa00
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-11-09 10:51:33 -0800

    Create python_linkedin_assessment.py

---
+++ python_linkedin_assessment.py
@@ -0,0 +1,11 @@
+# https://github.com/Ebazhanov/linkedin-skill-assessments-quizzes/blob/main/python/python-quiz.md
+  
+# Q1. What is an abstract class?
+# ANSWER: An abstract class exists only so that other "concrete" classes can inherit from the abstract class.
+
+# Q2. What happens when you use the build-in function any() on a list?
+# ANSWER: The any() function returns True if any item in the list evaluates to True. Otherwise, it returns False.
+
+# Q3. What data structure does a binary tree degenerate to if it isn't balanced properly?
+# ANSWER: linked list
+
