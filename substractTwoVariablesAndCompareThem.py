264d4025a8254245ce2528d17ce244168b6ae2e2
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-02 01:10:30 -0700

    Create substractTwoVariablesAndCompareThem.py

---
+++ substractTwoVariablesAndCompareThem.py
@@ -0,0 +1,8 @@
+#create a variable for first number
+numX = float(input("First-Number: "))
+#create a variable for second number
+numY = float(input("Second-Number: "))
+#create a variable that finds the substracts the first number from 2nd number
+subXandY = numX - numY
+#display the output as a string 
+print("Result: " + str(subXandY))
