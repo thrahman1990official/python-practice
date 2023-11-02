bd054d7ff85c366a246ddf7d401bb8f56925bbf0
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-02 01:51:40 -0700

    Create variousStringOperations.py

---
+++ variousStringOperations.py
@@ -0,0 +1,12 @@
+talhahsays = "I enjoy coding and I have ability to learn fast"
+#this will uppercase all the letters in the phrase
+print(talhahsays.upper())
+#this will lowercase all the letter in the phrase
+print(talhahsays.lower())
+#this will find the location of letter you want to search for
+print(talhahsays.find('g'))
+#this will replace the words with an another word
+print(talhahsays.replace('coding', 'programming'))
+print(talhahsays.replace('fast', 'quick'))
+#this will display location of the word based on the first letter of the word
+print(talhahsays.find("coding"))
