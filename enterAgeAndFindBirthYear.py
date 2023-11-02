083bb060311100da574ce588156f4383d3708151
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-02 00:39:28 -0700

    Create enterAgeAndFindBirthYear.py

---
+++ enterAgeAndFindBirthYear.py
@@ -0,0 +1,7 @@
+#first you set a variable for asking your age
+myAge = input("How old is Talha Rahman? ")
+#then you set variable for your year of birth in which the current year is substracted from your age
+myYearOfBirth = 2023 - int(myAge)
+#lastly you create a variable for displaying the result for your year of birth
+print("Your year of birth is: ")
+print(myYearOfBirth)
