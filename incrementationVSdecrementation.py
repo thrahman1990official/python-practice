78d27a736d2e41d9b409ebc7b807762d87d4dab1
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-02 22:15:24 -0700

    Create incrementationVSdecrementation.py

---
+++ incrementationVSdecrementation.py
@@ -0,0 +1,12 @@
+#first variable talhah assigned
+talhah = 13
+#second variable rahman assigned
+rahman = 99
+print("example of incrementation below")
+# below is talhah equal to talhah added to rahman
+talhah+=rahman
+print(int(talhah))
+print("example of decrementation below")
+#below is talha equal to talhah substracted from rahman
+talhah-=rahman
+print(int(talhah))
