0c0d27dad9911d834b73cdc9ba362760f7acd249
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-05 02:53:42 -0800

    Update comparisons.py

--- comparisons.py
+++ comparisons.py
@@ -1,20 +1,25 @@
 #if talhah is assigned as 5 is greater than 4
-talhah = 5>4
+talhah = 14>13
 #it should print true
 print(talhah)
 
 #if rahman is assigned as 5 not equal to 4
-rahman = 5!=4
+rahman = 14!=13
 #it should print true
 print(rahman)
 
 #if hamdur is assigned as 5 equal to 4
-hamdur = 5==4
+hamdur = 14==13
 #it should print false
 print(hamdur)
 
 #if rentTalhah is equal to 1500
 rentTalhah = 1500
-#it should print true in either case
+#it should print true in either case because rentTalhah is greater than 1000 or rentTalha is less than 2000
 print(rentTalhah > 1000 or rentTalhah < 2000)
+
+#if rentRahman is equal to 3000
+rentRahman = 3000
+#it should print false since rentRahman is not less than 2500 even though it is greater than 2000
+print(rentRahman > 2000 and rentRahman < 2500)
 
