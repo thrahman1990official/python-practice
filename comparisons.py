0c1ffa9c93e2df6524991e7c07c189e7f2038f5a
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-05 02:22:52 -0800

    Create comparisons.py

---
+++ comparisons.py
@@ -0,0 +1,15 @@
+#if talhah is assigned as 5 is greater than 4
+talhah = 5>4
+#it should print true
+print(talhah)
+
+#if rahman is assigned as 5 not equal to 4
+rahman = 5!=4
+#it should print true
+print(rahman)
+
+#if hamdur is assigned as 5 equal to 4
+hamdur = 5==4
+#it should print false
+print(hamdur)
+
