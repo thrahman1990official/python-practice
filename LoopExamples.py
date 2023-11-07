70c489bfa6753752d24852100eb25e361a84b756
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-06 21:36:01 -0800

    Update LoopExamples.py

--- LoopExamples.py
+++ LoopExamples.py
@@ -6,7 +6,12 @@ while talhah <= 18:
   talhah=talhah+1
 
 #example of for loop below
+#below is a list of temperatures
 temperatures = [13, 15, 17, 18, 19]
-
 for item in temperatures:
     print(item)
+
+#below is an example using for loop with range of numbers
+numbers = range(5,10)
+for number in numbers:
+    print(number)
