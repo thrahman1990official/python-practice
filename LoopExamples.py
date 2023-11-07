48388da1bb2ed951aa619cefd6c57c68fe3d8035
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-06 21:12:50 -0800

    Update LoopExamples.py

--- LoopExamples.py
+++ LoopExamples.py
@@ -4,3 +4,9 @@ while talhah <= 18:
   print(talhah)
   print(talhah*'talhah')
   talhah=talhah+1
+
+#example of for loop below
+temperatures = [13, 15, 17, 18, 19]
+
+for item in temperatures:
+    print(item)
