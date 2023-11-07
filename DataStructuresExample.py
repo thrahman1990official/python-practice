fbb328da78ef70665f79de56ab3a3d7e445369e5
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-06 21:57:49 -0800

    Create DataStructuresExample.py

---
+++ DataStructuresExample.py
@@ -0,0 +1,10 @@
+#list of the major tech companies
+techList = ["Cisco", "Microsoft", "Samsung", "Google", "Dell", "HP"]
+
+#if you want to let us say change the name of the second company on the list with another company
+techList[2] = "Tesla"
+
+print(techList)
+
+#if you want to remove Cisco from your list, this is how you do it
+techList.remove("Cisco")
