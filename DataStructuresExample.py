51b2f67bf2d879f38c7746e22b4fed6f8769db8d
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-06 22:21:34 -0800

    Update DataStructuresExample.py

--- DataStructuresExample.py
+++ DataStructuresExample.py
@@ -3,8 +3,12 @@ techList = ["Cisco", "Microsoft", "Samsung", "Google", "Dell", "HP"]
 
 #if you want to let us say change the name of the second company on the list with another company
 techList[2] = "Tesla"
-
 print(techList)
 
 #if you want to remove Cisco from your list, this is how you do it
 techList.remove("Cisco")
+print(techList)
+
+#if you want to insert a company such as Neflix between Samsung and Google
+techList.insert(3, "Tesla");
+print(techList)
