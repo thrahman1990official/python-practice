e6a7979c061b84691b43021815aa461bde5433db
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-23 11:08:27 -0800

    Update ACCESSING_CPU_RAM_HARD-DISK_STATS.py

--- ACCESSING_CPU_RAM_HARD-DISK_STATS.py
+++ ACCESSING_CPU_RAM_HARD-DISK_STATS.py
@@ -7,5 +7,8 @@ import psutil
 #cpu_count will give you number of cores in the CPU
 #logical=False will only give you the physical cores
 print(psutil.cpu_count(logical=False))
-#cpu_percent will give you number
+#cpu_percent will give you current cpu usage as a percentage
+#we need give an interval of 1 second for the cpu usage percentage
 print(psutil.cpu_percent(interval=1))
+#cpu_times will give you the seconds that cpu has spent as a user, nice, system, idle mode
+print(psutil.cpu_times())
