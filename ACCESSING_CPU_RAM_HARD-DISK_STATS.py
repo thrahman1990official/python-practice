2d80e1dba05537dd6cd499f12758f91bbda1b49b
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-23 10:48:43 -0800

    Update ACCESSING_CPU_RAM_HARD-DISK_STATS.py

--- ACCESSING_CPU_RAM_HARD-DISK_STATS.py
+++ ACCESSING_CPU_RAM_HARD-DISK_STATS.py
@@ -5,6 +5,7 @@ import psutil
 
 #CPU
 #cpu_count will give you number of cores in the CPU
+#logical=False will only give you the physical cores
 print(psutil.cpu_count(logical=False))
 #cpu_percent will give you number
 print(psutil.cpu_percent(interval=1))
