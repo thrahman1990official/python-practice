183c8fc78ca2bb27677dc9bd6695f9503d4b8816
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-23 10:31:17 -0800

    Create CPU_RAM_HARD-DISK_STATS.py

---
+++ CPU_RAM_HARD-DISK_STATS.py
@@ -0,0 +1,7 @@
+#psutil (process and system utilities) is a cross-platform library for retrieving information on running processes and system 
+#utilization (CPU, memory, disks, network, sensors) in Python. It is useful mainly for system monitoring, profiling and 
+#limiting process resources and management of running processes.
+import psutil
+
+#CPU
+print(psutil.cpu_count(logical=False))
