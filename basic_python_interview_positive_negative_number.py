f5d3d84e7cd833fda4f27cd30dd814633f510874
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-26 18:42:17 -0800

    Create basic_python_interview_positive_negative_number.py

---
+++ basic_python_interview_positive_negative_number.py
@@ -0,0 +1,10 @@
+#Write python program to find whether given number is positive or negative number
+
+number = int(input("Enter number to verify whether it is positive or negative or neither: "))
+if num > 0 :
+  print("positive number")
+elif num == 0:
+  print("zero")
+else:
+  print("negative number")
+
