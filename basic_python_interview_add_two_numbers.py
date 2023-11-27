3ede189178dce43402024103c524b1d50a06bbe3
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-26 17:51:32 -0800

    Update basic_python_interview_add_two_numbers.py

--- basic_python_interview_add_two_numbers.py
+++ basic_python_interview_add_two_numbers.py
@@ -1,6 +1,14 @@
 #Write Python program to add two numbers
+#first example is without input command with values already set for variables
 num1 = 70
 num2 = 50
 sum = num1 + num2
 
 print("sum of two numbers", sum)
+
+#second example is with the input command with customizable values set for variables
+num1 = int(input("enter 1st number: "))
+num2 = int(input("enter 2nd number: "))
+sum = num1 + num2
+print("sum of two numbers", sum)
+
