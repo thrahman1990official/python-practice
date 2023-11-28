f2c1aa2d6c5f5d2d71573329306d147626bbfea9
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-27 20:27:35 -0800

    Update basic_python_interview_verify_number_is_prime.py

--- basic_python_interview_verify_number_is_prime.py
+++ basic_python_interview_verify_number_is_prime.py
@@ -5,3 +5,13 @@
 #9 :- No. 1, 3, 9
 
 num = int(input("Enter number to verify if it is prime"))
+flag = False
+if num > 1:
+  for i in range(2, num//2)
+    if num % i == 0
+     flag = True
+      break
+if flag:
+   print(num, "is not prime number")
+else:
+   print(num, "is prime number")
