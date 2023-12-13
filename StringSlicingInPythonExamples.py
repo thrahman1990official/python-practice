fd0dfa5fe4a361cae892e2552375626ce16744b4
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-12-13 04:40:59 -0800

    Update StringSlicingInPythonExamples.py

--- StringSlicingInPythonExamples.py
+++ StringSlicingInPythonExamples.py
@@ -1,3 +1,7 @@
 # s[i:j] - slice of s from i to j
 # s[i:j:k] - slice of s from i to j with step k
 # s[startindex:endindex:step]
+
+s = "welcome to Talhah Rahman's python script samples"
+
+print(s[0])
