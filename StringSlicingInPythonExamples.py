0f3af0a1f9054aac8f1e4da9335e1cfcd9eb5e72
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-12-13 04:51:42 -0800

    Update StringSlicingInPythonExamples.py

--- StringSlicingInPythonExamples.py
+++ StringSlicingInPythonExamples.py
@@ -7,6 +7,13 @@ s = "welcome to Talhah Rahman's python script samples"
 #this should print welcome because w starts with 0 with total of 7 string arrays
 print(s[0:7])
 #should output welcome
-
+print(s[0:7:1])
+#should output welcome
 print(s[0:7:2])
 #should output wloe
+print(s[0:7:3])
+#should output wce
+print(s[0:7:4])
+#should output wo
+print(s[0:7:5])
+#should output wm
