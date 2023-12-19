3e919a33aca5316b47f2a2b31cc5bdf0b3361701
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-12-18 19:02:00 -0800

    Update basic_python_interview_question_factorial_numbers.py

--- basic_python_interview_question_factorial_numbers.py
+++ basic_python_interview_question_factorial_numbers.py
@@ -2,3 +2,5 @@
 # Factorial of number is multiplication
 # Of each and every number till given number except zero
 # Factorial of 3 = 1 * 2 * 3
+# 4 = 1 * 2 * 3 * 4 = 24
+# 5 = 1 * 2 * 3 * 4 * 5 = 120
