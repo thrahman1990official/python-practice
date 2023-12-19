7a604dd54a60cbb22e18e9a7339ddf138e0d36fa
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-12-18 19:36:34 -0800

    Update basic_python_interview_question_factorial_numbers.py

--- basic_python_interview_question_factorial_numbers.py
+++ basic_python_interview_question_factorial_numbers.py
@@ -4,3 +4,14 @@
 # Factorial of 3 = 1 * 2 * 3
 # 4 = 1 * 2 * 3 * 4 = 24
 # 5 = 1 * 2 * 3 * 4 * 5 = 120
+
+number = 5
+factorial = 1
+if number < 0:
+  print("Cannot use negative numbers or numbers less than zero to find factorial")
+elif number == 0:
+  print("Factorial of zero is 1")
+else:
+  for i in range(1, num+1):
+      factorial = factorial * i
+  print("Factorial of is", factorial)
