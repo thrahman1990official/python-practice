1c62ead1bf26cd27b475a23782d4737a39b79116
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-03-26 19:30:06 -0700

    Update Python_Network_Programming_Example1_Functions.py

--- Python_Network_Programming_Example1_Functions.py
+++ Python_Network_Programming_Example1_Functions.py
@@ -1,1 +1,10 @@
-
+//function example
+def device_type(device): //device_type will be the name of the function whereas device will be input argument
+    if device == '1800':
+      return "It's Cisco router"
+    if device == '2900':
+      return "It's Cisco router"
+    if device == '3750':
+      return "It's Cisco switch"
+    if device == '2950':
+      return "It's a Cisco switch"
