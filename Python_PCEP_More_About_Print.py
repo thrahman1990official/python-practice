27eb3a6da03786c4abf3d0e306e01bf5273eab2e
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-01 12:46:20 -0700

    Update Python_PCEP_More_About_Print.py

--- Python_PCEP_More_About_Print.py
+++ Python_PCEP_More_About_Print.py
@@ -2,5 +2,7 @@
 print('Hello!')
 # if a single quote is part of a word or sentence then you will need to use double quotes
 print("I'm studying Python")
+# or you can use single quotes in this format
+print('I\'m studying Python')
 
 
