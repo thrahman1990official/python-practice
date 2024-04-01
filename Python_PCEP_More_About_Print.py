a3bf5048c8da2d6ca9bad6505c50387ab0dfae2b
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-01 12:59:28 -0700

    Update Python_PCEP_More_About_Print.py

--- Python_PCEP_More_About_Print.py
+++ Python_PCEP_More_About_Print.py
@@ -1,14 +1,20 @@
 # print with single quotes around word
 print('Hello!')
-# output: Hello!
+# output: 
+# Hello!
 # if a single quote is part of a word or sentence then you will need to use double quotes
 print("I'm studying Python")
-# output: I'm studying Python
+# output: 
+# I'm studying Python
 # or you can use single quotes in this format
 print('I\'m studying Python')
 print('and having fun!')
 # output: 
 # I'm studying Python
 # and having fun!
+print('I\'m studying Python', 'and having fun!')
+# output:
+# I'm studying Python and having fun!
+
 
 
