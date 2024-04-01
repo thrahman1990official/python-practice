3255cebacbc8816139abee9e2abf0d69ef133e79
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-01 15:14:46 -0700

    Create Python_PCEP_reassigning_values.py

---
+++ Python_PCEP_reassigning_values.py
@@ -0,0 +1,18 @@
+# Below is an example where you are assigning variable age to a value and then creating a new variable
+# that adds the variable age to a number and prints the output
+age = 34
+new_age = age + 5
+print(new_age)
+# Output:
+# 39
+
+# Below is an example where you are assigning variable age to a value and then incrementing it by 7
+age = 34
+age = age + 7
+print(age)
+# Output:
+# 41
+
+
+
+
