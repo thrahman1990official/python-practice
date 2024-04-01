1764c163430e6b311570edd321315d31ae13f542
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-01 14:39:15 -0700

    Update Python_PCEP_numerical_representations.py

--- Python_PCEP_numerical_representations.py
+++ Python_PCEP_numerical_representations.py
@@ -1,3 +1,17 @@
 print(0.0000003)
 # Should give output in scientific notation format: 
 # 3e-07
+
+# Octal numbers
+# Start with 0o or 0O
+print(0o123)
+# Should give output in octal format:
+# 83
+
+# hexadecimal numbers
+# start with 0x or 0X
+print(0x123)
+# Should give output in hexadecimal format:
+# 291
+
+
