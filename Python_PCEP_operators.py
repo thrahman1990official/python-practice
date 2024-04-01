aca050d5fe269b975f9138bef2761341a3b50678
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-01 14:58:03 -0700

    Update Python_PCEP_operators.py

--- Python_PCEP_operators.py
+++ Python_PCEP_operators.py
@@ -1,1 +1,23 @@
+addition = 2 + 3
+print(addition)
 
+substraction = 5 - 4
+print(substraction)
+
+multiplication = 5 * 4
+print(multiplication)
+
+standardDivision = 7 / 2
+print(standardDivision)
+#NOTE: standard division produces a float number
+
+integerDivision = 7 // 2
+print(integerDivision)
+#NOTE: integer division produces a rounded number
+
+modulusDivision = 7 % 2
+print(modulusDivision)
+#NOTE: integer division produces a rounded remainder
+
+powerOperator = 2 ** 3
+print(powerOperator)
