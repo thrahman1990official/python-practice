3b7a960ed121897c7a45bc03d631ea827679f6be
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-01 17:40:56 -0700

    Update Python_PCEP_Input.py

--- Python_PCEP_Input.py
+++ Python_PCEP_Input.py
@@ -1,3 +1,6 @@
 print('What is your name?')
 username = input()
 print('Hello,', username)
+# A shorter way to write this is below:
+username = input('What is your name?')
+print('Hello,', username)
