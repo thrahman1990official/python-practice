3754b9bcb5856448dea97371f89ec25ecde9a54e
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-02 13:23:47 -0700

    Update Python_PCEP_Type_Casting.py

--- Python_PCEP_Type_Casting.py
+++ Python_PCEP_Type_Casting.py
@@ -4,3 +4,7 @@ print('Your height in feet is:', float_height_cm / 30.48)
 #the above three lines can be written like this below
 height_cm = float(input('Height converter: enter height in cm: '))
 print('Your height in feet is:', height_cm / 30.48)
+#another example using in
+year_born = int(input('What year were you born?'))
+print('In 2100, you will be', 2100 - year_born, 'years old, provided you live this long!')
+
