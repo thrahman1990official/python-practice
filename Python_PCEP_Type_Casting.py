e7a1f71742be63458349aa9709eeb1c17df3c46e
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-02 13:20:17 -0700

    Update Python_PCEP_Type_Casting.py

--- Python_PCEP_Type_Casting.py
+++ Python_PCEP_Type_Casting.py
@@ -1,3 +1,6 @@
 height_cm = input('Height converter: enter height in cm: ')
 float_height_cm = float(height_cm)
 print('Your height in feet is:', float_height_cm / 30.48)
+#the above three lines can be written like this below
+height_cm = float(input('Height converter: enter height in cm: '))
+print('Your height in feet is:', height_cm / 30.48)
