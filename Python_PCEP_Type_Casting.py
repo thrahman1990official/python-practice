cd189dfd66b2afb51b8768fd4b3775b4370f03e5
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-03 15:44:03 -0700

    Update Python_PCEP_Type_Casting.py

--- Python_PCEP_Type_Casting.py
+++ Python_PCEP_Type_Casting.py
@@ -7,4 +7,8 @@ print('Your height in feet is:', height_cm / 30.48)
 #another example using in
 year_born = int(input('What year were you born?'))
 print('In 2100, you will be', 2100 - year_born, 'years old, provided you live this long!')
-
+#another example of type casting
+temp_c = input('Enter temperature today in Celsius degree: ')
+temp_f = float(temp_c) * 1.8 + 32
+temp_statement = str(temp_c) + ' degrees Celsius equals ' + str(temp_f) + 'degrees Fahrenheit'
+print(temp_statement)
