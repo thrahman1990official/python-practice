02e38bd494c7f669e0c5fb93a5fe4fc7e34e41ff
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-04 11:00:18 -0700

    Update Python_PCEP_Operators.py

--- Python_PCEP_Operators.py
+++ Python_PCEP_Operators.py
@@ -3,3 +3,5 @@ print(+12)
 #should print 12 only without plus sign
 print(-12)
 #should print 12 only with minus sign
+print(2**2**3)
+#should print 256, it starts from right side 2 to the power of 3 and the output will the power of 2 resulting 2nd output
