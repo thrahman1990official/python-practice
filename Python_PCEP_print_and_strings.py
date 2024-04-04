d7f0f1af3a4f91c46e3f0dbc89497558d6e11fe4
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-04 12:55:20 -0700

    Create Python_PCEP_print_and_strings.py

---
+++ Python_PCEP_print_and_strings.py
@@ -0,0 +1,14 @@
+# Below are examples of different ways and doing prints and springs
+print('Talhahshah Rahmansultan')
+print('Python learner!')
+# Output:
+# Talhahshah Rahmansultan
+# Python learner!
+print('Talhahshah Rahmansultan', end='.')
+print('Python learner!')
+# Output:
+# Talhahshah Rahmansultan.Python learner!
+first_name = 'Talhahshah'
+print('Your first name is', first_nam, 'Welcome!', sep='-', end='=')
+# Output:
+# Your first name is-Talhahshah-Welcome!=
