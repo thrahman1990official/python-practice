2e7a3b5abe7269510f2357d73e39bebc09bcb55d
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-06 17:31:13 -0700

    Create Python_PCEP_Bit_Operators.py

---
+++ Python_PCEP_Bit_Operators.py
@@ -0,0 +1,14 @@
+first_bit = 1
+second_bit = 0
+
+print(first_bit & second_bit)
+
+first_bit = 0
+second_bit = 0
+
+print(first_bit | second_bit)
+
+first_bit = 0
+second_bit = 0
+
+print(first_bit * second_bit)
