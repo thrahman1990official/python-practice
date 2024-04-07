5c833bdb01663ff92ec017ef646f0bbdb1ca8e50
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-06 18:14:32 -0700

    Update Python_PCEP_Bit_Operators.py

--- Python_PCEP_Bit_Operators.py
+++ Python_PCEP_Bit_Operators.py
@@ -1,14 +1,20 @@
 first_bit = 1
 second_bit = 0
-#In Boolean algebra below is 1 LOGICAL AND 0 wHich results in 0
+#In Boolean algebra below is first_bit LOGICAL AND second_bit
 print(first_bit & second_bit)
 
 first_bit = 0
 second_bit = 0
-#In Boolean algebra below is 1 LOGICAL OR 0 which results in 1
+#In Boolean algebra below is first_bit LOGICAL OR 0
 print(first_bit | second_bit)
 
 first_bit = 0
 second_bit = 0
-#In Boolean algebra below is 1 logical XOR 0 which results in 0
+#In Boolean algebra below is 1 LOGICAL XOR 0 which results in 0
 print(first_bit ^ second_bit)
+
+first_bit = 0
+print(~first_bit)
+#In Boolean algebra below is LEFT-SHIFT OPERATOR of first_bit
+#in case of 0 it should output -1
+
