818222fefa8ed9e7a98c39cecbf40507fcb87f4d
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-08 14:02:48 -0700

    Update Python_PCEP_Bit_Operators.py

--- Python_PCEP_Bit_Operators.py
+++ Python_PCEP_Bit_Operators.py
@@ -13,23 +13,19 @@ second_bit = 0
 #In Boolean algebra below is 1 LOGICAL XOR 0 which results in 0
 print(first_bit ^ second_bit)
 
-number = 0
-print(~number)
+print(~0)
 #In Boolean algebra below is LEFT-SHIFT OPERATOR of number
 #in case of 0 it should output -1
 
-number = 12
-number << 1
+12 << 1
 #should output 24
 #12 * (2^1) = 24
 
-number = 12
-number << 2
+12 << 2
 #should output 48
 #12 * (2^2)
 
-number = 12
-number << 3
+12 << 3
 #should output 96
 #12 * (2^3)
 
