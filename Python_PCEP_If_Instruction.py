ef88c54c736df8ecb5f4b6d1e548f125dbcfa777
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-13 23:48:17 -0700

    Update Python_PCEP_If_Instruction.py

--- Python_PCEP_If_Instruction.py
+++ Python_PCEP_If_Instruction.py
@@ -2,3 +2,6 @@ user_age = int(input('What is your age?'))
 if user_age > 30:
   print('You are over 30 years old')
   print('Sorry, you do not qualify')
+else:
+  print('You are 30 years old or younger')
+  print('Congratulations, you qualify!')
