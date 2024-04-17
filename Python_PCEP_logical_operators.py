02ab1fa1e44066ad28550137c4e90ff14735d895
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-16 21:37:03 -0700

    Update Python_PCEP_logical_operators.py

--- Python_PCEP_logical_operators.py
+++ Python_PCEP_logical_operators.py
@@ -16,4 +16,17 @@ if True:
     print('Condition met')
 
 if False:
-    print('Condition met')
+    print('Condition met
+
+user_age = int(input('What's your age? '))
+
+if user_age > 30:
+    print('You are over 30 years old')
+    print('Sorry, you do not qualify')
+elif user_age == 30:
+    print('You are exactly 30 years old')
+    print('You will need to meet additional conditions to qualify')
+else:
+    print('You are 30 years old or younger')
+    print('Congratulations, you qualify!')
+    
