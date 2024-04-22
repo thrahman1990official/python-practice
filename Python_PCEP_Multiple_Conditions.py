34efa161ade8dee19c02cbd154f142ab10928c24
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-21 19:50:54 -0700

    Update Python_PCEP_Multiple_Conditions.py

--- Python_PCEP_Multiple_Conditions.py
+++ Python_PCEP_Multiple_Conditions.py
@@ -27,3 +27,7 @@ if not user_country == 'Germany':
   print('you are not from Germany!')
 else:
   print('you are from Germany')
+
+#Output:
+#Where do you come from? Germany
+#you are not from Germany!
