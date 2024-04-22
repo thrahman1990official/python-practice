64de6bd788b50ead85deed37caeacc64a5dcbe77
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-21 19:34:22 -0700

    Update Python_PCEP_Multiple_Conditions.py

--- Python_PCEP_Multiple_Conditions.py
+++ Python_PCEP_Multiple_Conditions.py
@@ -5,3 +5,16 @@ if user_age < 25 and user_country == 'Germany':
   print('You can apply for German student exchange programme')
 else:
   print('Sorry, you do not qualify')
+
+#Output:
+#What's your age? 26
+#What's your country? Germany
+#Sorry, you do not qualify
+
+user_country = input('What is your country?')
+
+if user_country == 'Sweden' or user_country == 'Denmark' or user_country == 'Norway':
+  print('You can apply for Scandinavian student exchange programme')
+else:
+  print('Sorry, you do not qualify')
+
