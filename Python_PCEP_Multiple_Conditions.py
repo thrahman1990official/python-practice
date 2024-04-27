6f7c39cc609065d8f17d481929933eef28219c88
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-26 19:06:30 -0700

    Update Python_PCEP_Multiple_Conditions.py

--- Python_PCEP_Multiple_Conditions.py
+++ Python_PCEP_Multiple_Conditions.py
@@ -31,3 +31,12 @@ else:
 #Output:
 #Where do you come from? Germany
 #you are not from Germany!
+
+user_age = int(input('What is your age? '))
+user_country = input('What is your country? ')
+
+if not user_country == 'Germany' and user_age < 25 or \
+    user_country == 'Germany' and user_age < 23:
+        print('You qualify!')
+else:
+        print('You don\'t qualify!')
