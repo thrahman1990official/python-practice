d522e0707da777bdcb61a4142d2172ca951ed5e3
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-28 20:29:22 -0700

    Create Python_PCEP_Nested_If_Statements.py

---
+++ Python_PCEP_Nested_If_Statements.py
@@ -0,0 +1,14 @@
+answer_a = input('Do you like traveling? y/n:')
+if answer_a == 'y':
+    answer_b == input('And do you like Asia? y/n: ')
+      if answer_b == 'y':
+          print('Excellent! You can win ticket to Thailand!')
+      else:
+          print('Sorry to hear that!')
+else:
+      print('Sorry to hear that!')
+
+# Output:
+# Do your like travelling? y/n: y
+# And do you like Asia? y/n: n
+# Sorry to hear that!
