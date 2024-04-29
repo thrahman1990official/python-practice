da496bb52f0f8e446724822b97bd8ae79bb3318a
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-29 07:14:18 -0700

    Update Python_PCEP_While_Loop.py

--- Python_PCEP_While_Loop.py
+++ Python_PCEP_While_Loop.py
@@ -22,6 +22,7 @@ while counter > 11:
     print(counter)
     counter += 1
 print('Finished!')
+# Output:
 # Finished!
 
 secret_number = 14
@@ -30,3 +31,10 @@ while user_input != secret_number:
     print('Wrong!')
     user_input = int(input('Try to guess secret number from 0 to 20: '))
 print('Perfect! You have guessed the secret number.')
+# Output:
+# Try to guess secret number from 0 to 20: 
+# 15
+# Wrong!
+# Try to guess secret number from 0 to 20: 
+# 14
+# Perfect! You have guessed the secret number.
