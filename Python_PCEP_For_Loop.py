2723ea031b50c206043555a9cc18b1018e691ce4
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-04-29 16:56:11 -0700

    Update Python_PCEP_For_Loop.py

--- Python_PCEP_For_Loop.py
+++ Python_PCEP_For_Loop.py
@@ -1,2 +1,9 @@
 for letter in 'hello!':
     print('Current letter:', letter)
+# Output:
+# Current letter: h
+# Current letter: e
+# Current letter: l
+# Current letter: l
+# Current letter: o
+# Current letter: !
