cdae7243351c8c263ce7af88420bd4a6cc52bcea
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-02 11:10:08 -0700

    Update Python_PCEP_For_Loop.py

--- Python_PCEP_For_Loop.py
+++ Python_PCEP_For_Loop.py
@@ -7,3 +7,21 @@ for letter in 'hello!':
 # Current letter: l
 # Current letter: o
 # Current letter: !
+
+counter = 1
+while counter < 11:
+    print(counter)
+    counter += 1
+print('Finished!')
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# Finished!
