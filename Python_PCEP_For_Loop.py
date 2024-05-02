bc293c115f625ad52c3148e92330ee264621b38e
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-02 11:22:23 -0700

    Update Python_PCEP_For_Loop.py

--- Python_PCEP_For_Loop.py
+++ Python_PCEP_For_Loop.py
@@ -25,3 +25,47 @@ print('Finished!')
 # 9
 # 10
 # Finished!
+
+for counter in range(1,11):
+    print(counter)
+print('Finished!')
+# 1 is the start value and 11 is the stop value
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# Finished!
+# if you remove 1 then a default value of 0 will be used
+for counter in range(11):
+    print(counter)
+print('Finished!')
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# Finished!
+for counter in range(1, 11, 2):
+    print(counter)
+print('Finished!')
+# 1 will be start value , 11 will be stop value , and 2 will be the incremented increase
+# Output:
+# 1
+# 3
+# 5
+# 7
+# 9
+# Finished!
