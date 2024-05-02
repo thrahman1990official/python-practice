ff1719920bca755b14191be9cbc9c0a26ee0830c
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-02 11:39:16 -0700

    Update Python_PCEP_Break_And_Continue.py

--- Python_PCEP_Break_And_Continue.py
+++ Python_PCEP_Break_And_Continue.py
@@ -6,5 +6,24 @@ while True:
 # Output:
 # Enter your name or EXIT to close program: Talhahshah
 # Hello Talhahshah
-# Enter your name or EXIT to close program: EXIT
-# Program terminates when typing EXIT
+for i in range(1, 21):
+    if i % 5 == 0:
+        continue
+    print(i)
+# Output below:   
+# 1
+# 2
+# 3
+# 4
+# 6
+# 7
+# 8
+# 9
+# 11
+# 12
+# 13
+# 14
+# 16
+# 17
+# 18
+# 19
