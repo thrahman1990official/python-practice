6a2021bd376eaba2cd90303cd80cea498b84ae0c
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-02 14:52:13 -0700

    Update Python_PCEP_Loops_Additional.py

--- Python_PCEP_Loops_Additional.py
+++ Python_PCEP_Loops_Additional.py
@@ -1,4 +1,7 @@
-for i in range(11)
+for i in range(11):
     pass
 # pass instruction will prevent the code from throwing errors
 # below is example of simple multipication table from one to five
+for a in range(1,6):
+    for b in range(1,6):
+         print(a, 'x', b, '=', a * b)
