dfc0f9bcfb803a17195ede69b1a9ddfa6e55af4c
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-02 11:34:49 -0700

    Update Python_PCEP_Break_And_Continue.py

--- Python_PCEP_Break_And_Continue.py
+++ Python_PCEP_Break_And_Continue.py
@@ -3,3 +3,8 @@ while True:
     if (name == 'EXIT'):
         break
     print('Hello', name)
+# Output:
+# Enter your name or EXIT to close program: Talhahshah
+# Hello Talhahshah
+# Enter your name or EXIT to close program: EXIT
+# Program terminates when typing EXIT
