be79bc6b1e04ffcb0f4d810daa3ecd1260fe1a5d
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 16:15:56 -0700

    Update Python_PCEP_List_Checking_Presence.py

--- Python_PCEP_List_Checking_Presence.py
+++ Python_PCEP_List_Checking_Presence.py
@@ -1,6 +1,5 @@
 for char in 'happy message':
     print(char)
-
 # Output:
 # h
 # a
@@ -15,3 +14,19 @@ for char in 'happy message':
 # a
 # g
 # e
+
+invited_guests = ['Kate', 'Adam', 'Kerry', 'Joe', 'Anne', 'Marie']
+name = input('What is your name? ')
+if name in invited_guests:
+    print('Welcome!')
+else:
+    print('You are not invited!')
+# input:
+# What is your name? Talhah
+# You are not invited!
+invited_guests = ['Kate', 'Adam', 'Kerry', 'Joe', 'Anne', 'Marie']
+name = input('What is your name? ')
+if name not in invited_guests:
+    print('You are not invited!')
+else:
+    print('Welcome!')
