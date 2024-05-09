b65dd3ce2b017d67c2e8b9661e8b0f742f92f62d
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 15:24:21 -0700

    Update Python_PCEP_list_change_positions.py

--- Python_PCEP_list_change_positions.py
+++ Python_PCEP_list_change_positions.py
@@ -25,3 +25,9 @@ top_cities[0], top_cities[4] = top_cities[4], top_cities[0]
 print(top_cities)
 # Output:
 # ['Phoenix', 'Los Angeles', 'Chicago', 'Houston', 'New York City']
+
+top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+top_cities.sort()
+print(top_cities)
+# Output:
+# ['Chicago', 'Houston', 'Los Angeles', 'New York City', 'Phoenix']
