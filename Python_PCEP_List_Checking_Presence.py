9ca439d4a407dbe7370464c259ea68ed4d04c476
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 16:17:09 -0700

    Update Python_PCEP_List_Checking_Presence.py

--- Python_PCEP_List_Checking_Presence.py
+++ Python_PCEP_List_Checking_Presence.py
@@ -21,9 +21,11 @@ if name in invited_guests:
     print('Welcome!')
 else:
     print('You are not invited!')
-# input:
+# Input:
 # What is your name? Talhah
+# Output:
 # You are not invited!
+# Below is another way to have the desired output above
 invited_guests = ['Kate', 'Adam', 'Kerry', 'Joe', 'Anne', 'Marie']
 name = input('What is your name? ')
 if name not in invited_guests:
