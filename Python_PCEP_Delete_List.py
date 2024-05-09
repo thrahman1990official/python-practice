687bbc737c81abde8150a55bcd12e3887c901a14
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 11:33:45 -0700

    Update Python_PCEP_Delete_List.py

--- Python_PCEP_Delete_List.py
+++ Python_PCEP_Delete_List.py
@@ -1,7 +1,12 @@
 top_cities = ['New York City', 'Los Angeles', 'Detroit', 'Chicago', 'Oakland', 'Miami']
-# Below command should delete Detroit from the list
+# Below command should delete Detroit from the list since it is in index location 2
 del top_cities[2]
 print(top_cities)
 # Output:
 # ['New York City', 'Los Angeles', 'Chicago', 'Oakland', 'Miami']
+# Below command should delete all cities starting from index location 3
+del top_cities[3:]
+print(top_cities)
+# Output:
+# ['New York City', 'Los Angeles', 'Detroit']
 
