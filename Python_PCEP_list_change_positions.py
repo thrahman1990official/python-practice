e060afdbcfcb64d2cf82fa8b7c9a912e82440c93
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 14:15:30 -0700

    Create Python_PCEP_list_change_positions.py

---
+++ Python_PCEP_list_change_positions.py
@@ -0,0 +1,6 @@
+first = input('Enter 1st number: ')
+second = input('Enter 2nd number: ')
+print('Before swapping:', first, second)
+first = second
+second = first
+print('After swapping:' first, second)
