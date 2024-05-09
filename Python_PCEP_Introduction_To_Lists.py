ebdfa5c6ef08af5c4df4c3b7158675bc4423d1bd
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 10:53:16 -0700

    Update Python_PCEP_Introduction_To_Lists.py

--- Python_PCEP_Introduction_To_Lists.py
+++ Python_PCEP_Introduction_To_Lists.py
@@ -8,3 +8,12 @@ print(top_cities)
 
 # Output:
 # ['New York City', 'Los Angeles', 'Chicago']
+
+# To get the first element which is New York City in this case which is in list location 0
+print(top_cities[0])
+# Output:
+# New York City
+# To get the second element which is Los Angeles in this case which is in list location 1
+print(top_cities[1])
+# Output:
+# Los Angeles
