914cfdb00bc2aafcbde8f807fb637d6cafbbfb48
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 13:35:05 -0700

    Update Python_PCEP_Iterating_Lists.py

--- Python_PCEP_Iterating_Lists.py
+++ Python_PCEP_Iterating_Lists.py
@@ -1,11 +1,20 @@
 top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
 for city in top_cities:
     print('Current city:', city)
-
 # Output:
 # Current city: New York City
 # Current city: Los Angeles
 # Current city: Chicago
 # Current city: Houston
 # Current city: Phoenix
+top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+for city_index in range(len(top_cities)):
+    print('Current index:', city_index, '| Current city:', top_cities[city_index], )
+# Output:
+# Current index: 0 | Current city: New York City
+# Current index: 1 | Current city: Los Angeles
+# Current index: 2 | Current city: Chicago
+# Current index: 3 | Current city: Houston
+# Current index: 4 | Current city: Phoenix
+
 
