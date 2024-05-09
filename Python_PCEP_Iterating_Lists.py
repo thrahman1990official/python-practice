2fdd0fb899174ad3d125999502a71c8b5c70e3b1
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 13:45:50 -0700

    Update Python_PCEP_Iterating_Lists.py

--- Python_PCEP_Iterating_Lists.py
+++ Python_PCEP_Iterating_Lists.py
@@ -17,4 +17,12 @@ for city_index in range(len(top_cities)):
 # Current index: 3 | Current city: Houston
 # Current index: 4 | Current city: Phoenix
 
+spendings = [32.45, 18.65, 23.45, 78.32, 5.23]
+sum = 0.0
+for spending in spendings:
+    sum += spending
+print('Money spent:', sum)
+# Output:
+# Money spent: 158.1
+
 
