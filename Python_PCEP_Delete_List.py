8e169536dec538f073c91eb1dcb61dee54a241a9
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 11:27:06 -0700

    Create Python_PCEP_Delete_List.py

---
+++ Python_PCEP_Delete_List.py
@@ -0,0 +1,7 @@
+top_cities = ['New York City', 'Los Angeles', 'Detroit', 'Chicago', 'Oakland', 'Miami']
+# Below command should delete Detroit from the list
+del top_cities[2]
+print(top_cities)
+# Output:
+# ['New York City', 'Los Angeles', 'Chicago', 'Oakland', 'Miami']
+
