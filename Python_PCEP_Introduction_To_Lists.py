5ce00b0f96a8439d1271f4244f4bad7e31e46128
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 11:03:46 -0700

    Update Python_PCEP_Introduction_To_Lists.py

--- Python_PCEP_Introduction_To_Lists.py
+++ Python_PCEP_Introduction_To_Lists.py
@@ -1,14 +1,11 @@
 city_1 = 'New York City'
 city_2 = 'Los Angeles'
 city_3 = 'Chicago'
-
 empty_list = []
 top_cities = ['New York City', 'Los Angeles', 'Chicago']
 print(top_cities)
-
 # Output:
 # ['New York City', 'Los Angeles', 'Chicago']
-
 # To get the first element which is New York City in this case which is in list location 0
 print(top_cities[0])
 # Output:
@@ -17,3 +14,8 @@ print(top_cities[0])
 print(top_cities[1])
 # Output:
 # Los Angeles
+# To get the last element on this list which in this case is Chicago
+print(top_cities[-1])
+# Output:
+# Chicago
+
