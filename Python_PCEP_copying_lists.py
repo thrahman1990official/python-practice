f23a9d7d4cd2db2bb1f98f1b4ad65e41505076e8
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-10 10:52:23 -0700

    Update Python_PCEP_copying_lists.py

--- Python_PCEP_copying_lists.py
+++ Python_PCEP_copying_lists.py
@@ -10,7 +10,14 @@ list_original = [1, 2, 3]
 list_new = list_original
 list_original[0] = -5
 print('Original:', list_original, '\nNew:', list_new)
-
 # Output:
 # Original: [-5, 2, 3]
 # New: [-5, 2, 3]
+#---------------------------------------------------------------
+# Below is an example of the same above code except with slicing
+list_original = [1, 2, 3]
+list_new = list_original[:]
+list_original[0] = -5
+print('Original:', list_original, '\nNew:', list_new)
+Original: [-5, 2, 3]
+New: [1, 2, 3]
