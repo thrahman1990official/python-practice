56591a6fee704031883f0cca6defc3af7c93e40b
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-10 13:13:02 -0700

    Update Python_PCEP_nested_lists.py

--- Python_PCEP_nested_lists.py
+++ Python_PCEP_nested_lists.py
@@ -3,6 +3,8 @@ countries = ['UK', 'US', 'Germany']
 countries = [1, 'UK', 2, 'US']
 cells = [['A1', 'A2', 'A3'], ['B1', 'B2', 'B3']]
 print(cells[0])
-
 # Output:
 # ['A1', 'A2', 'A3']
+print(cells[0][0])
+# Output:
+# A1
