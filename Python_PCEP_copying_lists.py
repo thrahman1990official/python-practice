83787a6c8698cd515946057350a215f53119cbec
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-10 10:32:24 -0700

    Update Python_PCEP_copying_lists.py

--- Python_PCEP_copying_lists.py
+++ Python_PCEP_copying_lists.py
@@ -10,3 +10,7 @@ list_original = [1, 2, 3]
 list_new = list_original
 list_original[0] = -5
 print('Original:', list_original, '\nNew:', list_new)
+
+# Output:
+# Original: [-5, 2, 3]
+# New: [-5, 2, 3]
