bac8c3c8e91df4f016409c5c8f7d75cda56bdc6f
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-09 17:37:14 -0700

    Create Python_PCEP_copying_lists.py

---
+++ Python_PCEP_copying_lists.py
@@ -0,0 +1,10 @@
+name_original = 'Talhahshah'
+name_new = name_original
+name_original = 'Rahmansultan'
+print(name_original, name_new)
+
+# Output:
+# Rahmansultan Talhahshah
+
+list_original = [1, 2, 3]
+list_new = list_original
