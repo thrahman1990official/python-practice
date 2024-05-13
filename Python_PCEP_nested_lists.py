61d8e2e459cddf20c3f0e31f087aee0a61d19713
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-13 10:16:21 -0700

    Update Python_PCEP_nested_lists.py

--- Python_PCEP_nested_lists.py
+++ Python_PCEP_nested_lists.py
@@ -54,7 +54,7 @@ for row in table:
 # A1 A2 A3
 # B1 B2 B3
 
-table = [i for i in range(1,6)]
+table = [[i for i in range(1,6)] for j in range(4)]
 print(table)
 # Output:
-# [1, 2, 3, 4, 5]
+# [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
