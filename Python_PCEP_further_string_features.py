ff04a46d75c52c45e5b0339c7be03c9c510015a3
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-13 11:37:45 -0700

    Update Python_PCEP_further_string_features.py

--- Python_PCEP_further_string_features.py
+++ Python_PCEP_further_string_features.py
@@ -11,6 +11,17 @@ print(fav_band[:4])
 # Output:
 # Dark
 
+user_number = input('Please provide number: ')
+if user_number.isnumeric():
+    print('Thank you, that\'s correct number!')
+else:
+    print('Sorry,', user_number, 'is not a number!')
+# Input:
+# Please provide number:
+# 15
+# Output:
+# Thank you, that's correct number!
+
 text = 'Hail Lord Allah The King Of Heavens and Earth'
 text_cap = text.upper()
 print(text_cap)
