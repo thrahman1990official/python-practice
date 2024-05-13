b1df08369c9167e1462e722290046249340140ae
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-13 11:23:55 -0700

    Update Python_PCEP_further_string_features.py

--- Python_PCEP_further_string_features.py
+++ Python_PCEP_further_string_features.py
@@ -1,4 +1,12 @@
 fav_band = 'Darkroom Familia'
-print(fav_band[6])
+# Below print statement will only print letter at the index #4 of the string
+print(fav_band[4])
 # Output:
-# o
+# r
+
+fav_band = 'Darkroom Familia'
+# Below print statement will print letters that are before index #4 of the string
+# Meaning letters starting from #0 till #3 of the index
+print(fav_band[:4])
+# Output:
+# Dark
