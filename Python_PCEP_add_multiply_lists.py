09e906bc9b8a55d65271d81ffc84fa9ab797ea4d
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-13 10:50:37 -0700

    Create Python_PCEP_add_multiply_lists.py

---
+++ Python_PCEP_add_multiply_lists.py
@@ -0,0 +1,7 @@
+list_us = ['New York City', 'Chicago']
+list_uk = ['London', 'Bristol']
+list_all = list_us + list_uk
+print(list_all)
+
+# Output:
+# ['New York City', 'Chicago', 'London', 'Bristol']
