006da48d5bb5d424d5d39abb971ca24a1eb94bc2
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-15 14:54:24 -0700

    Create Python_PCEP_Intro_Tuples.py

---
+++ Python_PCEP_Intro_Tuples.py
@@ -0,0 +1,12 @@
+#four ways of writing tuples
+empty_tuple = ()
+one_el_tuple_a = (1,)
+one_el_tuple_b = 1,
+three_el_tuple = 1, 2, 3,
+print(three_el_tuple)
+# Output:
+# (1, 2, 3)
+
+# two types of python data: mutable and immutable data
+# mutable data can be freely update anytime you want
+# list are a good example of mutable data
