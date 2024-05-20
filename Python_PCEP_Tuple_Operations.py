d492e5deab27a5ec386f80d3ce55bc6aa5cde9fe
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-19 22:30:58 -0700

    Create Python_PCEP_Tuple_Operations.py

---
+++ Python_PCEP_Tuple_Operations.py
@@ -0,0 +1,11 @@
+user_data = ('Talhahshah', 'Talhahstan', 1990)
+print(len(user_data))
+# Should print out the number of contents inside the tuple
+# Output:
+# 3
+
+user_data = ('Talhahshah', 'Talhahstan', 1990)
+if 'Talhahstan' in user_data:
+    print('This person comes from Talhahstan.')
+# Output:
+# This person comes from Talhahstan.
