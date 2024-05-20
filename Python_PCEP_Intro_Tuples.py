163694b41e9117662a4f953a979473288ce00374
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-19 22:21:32 -0700

    Update Python_PCEP_Intro_Tuples.py

--- Python_PCEP_Intro_Tuples.py
+++ Python_PCEP_Intro_Tuples.py
@@ -7,6 +7,17 @@ print(three_el_tuple)
 # Output:
 # (1, 2, 3)
 
-# two types of python data: mutable and immutable data
-# mutable data can be freely update anytime you want
-# list are a good example of mutable data
+# Tuples prefer round brackets, while lists prefer square brackets
+# Both type of collections can have many elements separated by commas
+# Mutable data can be freely updated anytime you want
+# Two types of python data: mutable and immutable data
+# List are a good example of mutable data
+# When you create a list, you can easily add new elements to it, delete unwanted elements swap the places
+
+user_data = ('Talhahshah', 'Talhahstan', 1990)
+user_data = ('Rahmansultan', 'Rahmannation', 2000)
+
+
+
+
+
