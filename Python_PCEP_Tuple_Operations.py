318ca2288272d43807459c88c3d31595f27a3ff5
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-19 23:03:09 -0700

    Update Python_PCEP_Tuple_Operations.py

--- Python_PCEP_Tuple_Operations.py
+++ Python_PCEP_Tuple_Operations.py
@@ -9,3 +9,11 @@ if 'Talhahstan' in user_data:
     print('This person comes from Talhahstan.')
 # Output:
 # This person comes from Talhahstan.
+
+user_data = ('Talhahshah', 'Talhahstan', 1990)
+for element in user_data:
+    print(element)
+# Output:
+# Talhahshah
+# Talhahstan
+# 1990
