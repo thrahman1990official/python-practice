ed7d7d688360af3f9a20ba8e84f9788db3dc9c76
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-21 16:55:18 -0700

    Update Python_PCEP_Tuples_In_Lists_Lists_In_Tuples.py

--- Python_PCEP_Tuples_In_Lists_Lists_In_Tuples.py
+++ Python_PCEP_Tuples_In_Lists_Lists_In_Tuples.py
@@ -1,4 +1,4 @@
 # below is an example of tuple in a list
 capitals = [('London', 'UK', 8.98), ('Canberra', 'Australia', 0.4), ('Algiers', 'Algeria', 3.9)]
 for capital in capitals:
-    print('Name:', capital[0], ', Country:', capital[1], ',Population:', capital[2])
+    print('Name:', capital[0], ', Country:', capital[1], ', Population:', capital[2])
