dfddb156af67623cfff988d92a5bd8c5918e10dd
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-21 15:06:40 -0700

    Update Python_PCEP_Tuple_Operations.py

--- Python_PCEP_Tuple_Operations.py
+++ Python_PCEP_Tuple_Operations.py
@@ -27,3 +27,16 @@ numbers = (0,1) * 10
 print(numbers)
 # Output:
 # (0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1)
+
+# Below is an example of list where all are names and all represent male names
+male_name = ['Hamzah', 'Omar', 'Sohaib']
+# Each element is a float that represents temperature in balloon on different day
+# Again, all elements are floats and all of them represent temperatures
+berlin_temp = [13.0, 17.5, 12.0]
+
+# Difference between Tuples and Lists?
+# Other than the fact that Tuples use () brackets and Lists use [] 
+# Tuples are often used when you want to group together values of different types that are
+# somehow related and together they form some sort of structure some sort of bigger data
+# You have seen one such example already when we had user data variable
+# Here you can see that we have three elements of different data types
