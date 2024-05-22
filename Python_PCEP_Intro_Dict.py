0820452bffa36942f6e4501792a83991996cb239
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-22 11:48:17 -0700

    Create Python_PCEP_Intro_Dict.py

---
+++ Python_PCEP_Intro_Dict.py
@@ -0,0 +1,7 @@
+# Below is an example of Dictionaties
+# Dictionaries are collections used to store key value pairs
+emails = {
+'Anne Stahl': 'astahl@gmail.com',
+'Peter Small': 'peters@yandex.com',
+'Mark Steel': 'mark@steel.com'
+}
