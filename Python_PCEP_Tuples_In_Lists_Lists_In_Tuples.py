1ee9561278adb8852f0b066fbe8e9776d9cc1194
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-21 17:00:35 -0700

    Update Python_PCEP_Tuples_In_Lists_Lists_In_Tuples.py

--- Python_PCEP_Tuples_In_Lists_Lists_In_Tuples.py
+++ Python_PCEP_Tuples_In_Lists_Lists_In_Tuples.py
@@ -2,3 +2,8 @@
 capitals = [('London', 'UK', 8.98), ('Canberra', 'Australia', 0.4), ('Algiers', 'Algeria', 3.9)]
 for capital in capitals:
     print('Name:', capital[0], ', Country:', capital[1], ', Population:', capital[2])
+
+# Output:    
+# Name: London , Country: UK , Population: 8.98
+# Name: Canberra , Country: Australia , Population: 0.4
+# Name: Algiers , Country: Algeria , Population: 3.9
