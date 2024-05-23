d82a284205300cec649db9556ce376928b70b02f
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-23 16:58:47 -0700

    Update Python_PCEP_dict_operations.py

--- Python_PCEP_dict_operations.py
+++ Python_PCEP_dict_operations.py
@@ -7,9 +7,15 @@ print(grades)
 # {'John': 'A-', 'Anne': 'B'}
 grades['Anne'] = 'A'
 print(grades)
+# Replace the grade of Anne from B to A
 # Output:
 # {'John': 'A-', 'Anne': 'A'}
 grades.update({'John':'A'})
 print(grades)
+# Updates the value of John
 # Output:
 # {'John': 'A', 'Anne': 'A'}
+len(grades)
+# Returns the number of entries in the dictionary, so in this case, one if John and the other is Anne
+# Output:
+# 2
