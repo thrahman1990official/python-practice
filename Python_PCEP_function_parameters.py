cf63ffd24cd7fa1a29a907d6a73d0e911919ea2a
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-28 13:30:59 -0700

    Update Python_PCEP_function_parameters.py

--- Python_PCEP_function_parameters.py
+++ Python_PCEP_function_parameters.py
@@ -7,4 +7,12 @@ def get_average(input_numbers):
 get_average([5.0, 3.5, 7.8, 9.9, 10.0])
 # Output:
 # 7.24
+
+def print_letter_count(text, letter):
+    counter = 0
+    for char in text:
+        if char == letter:
+            counter += 1
+    print('Number of', letter, 'is', counter)
+print_letter_count('Welcome', 'e')
   
