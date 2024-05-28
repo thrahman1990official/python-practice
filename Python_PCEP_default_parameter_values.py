291a0265b4616a8e8225a4b00f2ce11ad301634a
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-28 16:29:06 -0700

    Update Python_PCEP_default_parameter_values.py

--- Python_PCEP_default_parameter_values.py
+++ Python_PCEP_default_parameter_values.py
@@ -1,3 +1,13 @@
 print('Hello', 'How are you?', end='.', sep='-')
 # Output:
 # Hello-How are you?.
+
+def print_letter_count(text='This is the default string to search', letter='a'):
+    counter = 0
+    for char in text:
+        if char == letter:
+            counter += 1
+    print('Number of', letter, 'is', counter)
+print_letter_count('How many letters are here?')
+# Number of a is 3
+print_letter_count()
