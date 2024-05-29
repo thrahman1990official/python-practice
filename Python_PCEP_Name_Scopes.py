4d15042dca00f8077c80e2e478feb7faa877a863
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-29 15:02:54 -0700

    Update Python_PCEP_Name_Scopes.py

--- Python_PCEP_Name_Scopes.py
+++ Python_PCEP_Name_Scopes.py
@@ -1,5 +1,10 @@
 # Example of Name Scope
 def show_truth():
+    print(mysterious_var)
+mysterious_var = 'Surprise!'
+show_truth()
+
+def show_truth():
     mysterious_var = 'Surprise!'
     print(mysterious_var)
 show_truth()
