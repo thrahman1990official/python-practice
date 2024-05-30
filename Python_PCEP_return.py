be5b415bb86a5d9da79258eddecc86192e1893a3
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-30 15:52:52 -0700

    Update Python_PCEP_return.py

--- Python_PCEP_return.py
+++ Python_PCEP_return.py
@@ -2,5 +2,7 @@ def get_average(input_numbers):
     sum = 0.0
     for number in input_numbers:
         sum += number
-    average = sum / len(input_number)
+    average = sum / len(input_numbers)
     return average
+print('The average is:', get_average([5.0, 3.5, 7.8, 9.9, 10.0]))
+# The average is: 7.24
