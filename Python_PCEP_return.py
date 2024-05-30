795bd30e937ddc68f950ddf769723a76120765ae
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-05-30 16:09:44 -0700

    Update Python_PCEP_return.py

--- Python_PCEP_return.py
+++ Python_PCEP_return.py
@@ -10,3 +10,17 @@ print('The average is:', get_average([5.0, 3.5, 7.8, 9.9, 10.0]))
 average = get_average([5.0, 3.5, 7.8, 9.9, 10.0])
 if average > 5.0:
     print('The average is too high!')
+# Output:
+# The average is too high!
+def get_average(input_numbers):
+    sum = 0.0
+    for number in input_numbers:
+        sum += number
+    average = sum / len(input_numbers)
+    return average
+    print('Show me!')
+
+    get_average([2])
+
+    get_average
+
