ca8d9dfbff55dcfe7ebfc02a62e57ec8e1d68d91
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-06-04 15:35:17 -0700

    Update Python_PCEP_exceptions_intro.py

--- Python_PCEP_exceptions_intro.py
+++ Python_PCEP_exceptions_intro.py
@@ -4,9 +4,18 @@ try:
     print('The inverse of', value, 'is', 1/value)
 except:
     print('You did not provide a number, so I will not calculate the inverse')
+# run the above code 1st time
 # input:
 # Enter an integer:
 # a
 # output:
 # You did not provide a number, so I will not calculate the inverse
+# run the above code 2nd time
+# Input:
+# Enter an integer:
+# 2
+# output:
+# The inverse of 2 is 0.5
+
+
   
