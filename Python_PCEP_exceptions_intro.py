413ce609cab5826318d4bcc87e4ddc548224da5e
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-06-04 15:20:53 -0700

    Update Python_PCEP_exceptions_intro.py

--- Python_PCEP_exceptions_intro.py
+++ Python_PCEP_exceptions_intro.py
@@ -1,2 +1,7 @@
-value = int(input('Enter an integer: '))
-print('The inverse of', value, 'is', 1/value)
+# below is an example of an exception
+try:
+    value = int(input('Enter an integer: '))
+    print('The inverse of', value, 'is', 1/value)
+except:
+    print('You did not provide a number, ')
+  
