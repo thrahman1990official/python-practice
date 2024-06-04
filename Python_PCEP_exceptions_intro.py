a9f9489023fd883be3bab6eeea4474a8ed0b7db4
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-06-04 15:23:40 -0700

    Update Python_PCEP_exceptions_intro.py

--- Python_PCEP_exceptions_intro.py
+++ Python_PCEP_exceptions_intro.py
@@ -3,5 +3,5 @@ try:
     value = int(input('Enter an integer: '))
     print('The inverse of', value, 'is', 1/value)
 except:
-    print('You did not provide a number, ')
+    print('You did not provide a number, so I will not calculate the inverse')
   
