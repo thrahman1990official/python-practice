49a1f16ac386b86084768defc324866509d00f7d
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-06-10 12:41:07 -0700

    Update Python_PCEP_exception_propagation.py

--- Python_PCEP_exception_propagation.py
+++ Python_PCEP_exception_propagation.py
@@ -1,3 +1,11 @@
 def get_day(user_info):
   day = int(input('What is the day of your bday? '))
-  user.info.append(day) 
+  user_info.append(day) 
+  
+def get_month(user_info):
+  month = int(input('What is the month (1-12) of your bday? '))
+  user_info.append(month)
+
+def get_year(user_info):
+  year = int(input('What is the year of your bday? '))
+  
