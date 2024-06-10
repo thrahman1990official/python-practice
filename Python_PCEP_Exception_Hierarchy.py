73d0ab91ad945f9c85633d3c15b7306297ec15cb
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-06-10 11:05:34 -0700

    Create Python_PCEP_Exception_Hierarchy.py

---
+++ Python_PCEP_Exception_Hierarchy.py
@@ -0,0 +1,8 @@
+import sys
+
+user_name = input('What's your name? ')
+if user_name == 
+  print('Empty name? I cannot work with that. I am closing program. Bye!')
+  sys.exit()
+print('Hello,', user_name)
+print('Let us get started...')
